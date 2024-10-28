089640890542099439cc14fb1aebe98f3df2d111
Tatiana Shimanskya <ttnsimanskaa@gmail.com>
2024-10-28 11:39:11 +0300

    init tests

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="extreme",
+    version="1.0.0",
+    description="Extreme programming training project",
+    packages=["extreme"],
+    install_requires=["Flask==3.0.3"],
+)
